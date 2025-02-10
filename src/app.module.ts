46c0973d625c8b1b8800fc4049f7f1b2038c97cf
yoruwitch <evelyn.fernandes4@gmail.com>
2025-02-10 09:25:43 -0300

    feat: integrate PostgreSQL with TypeORM and add configuration service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/db.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TaskModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/task/task.entity.ts
+++ src/task/task.entity.ts
@@ -1,8 +1,29 @@
-// @Entity
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'tasks' })
 export class TaskEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'title', length: 100, nullable: false })
   title: string;
+
+  @Column({ name: 'title', length: 150, nullable: false })
   description: string;
-  // status: number;
-  // statusDescription: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
